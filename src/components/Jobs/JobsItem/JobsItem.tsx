7e222cfffc4f78686eac931e6ad66626b9e1acb6
Oleg Gnatyk <103590840+GnatykOleg@users.noreply.github.com>
2023-01-29 17:11:22 +0200

    Add adress to cards

--- src/components/Jobs/JobsItem/JobsItem.tsx
+++ src/components/Jobs/JobsItem/JobsItem.tsx
@@ -13,46 +13,50 @@ const JobsItem: FC = () => {
 
   return (
     <>
-      {data.map(({ id, createdAt, title, pictures, location, name }) => {
-        return (
-          <li
-            key={id}
-            className="transition-all hover:scale-105 bg-cardBg xlMin:bg-white shadow-lg mb-2 last:mb-0  rounded-lg px-4 pt-[13px] xlMin:py-[24px] pb-[27px] xlMin:relative"
-          >
-            <Link className="flex" to={`/jobs-list/${id}`}>
-              <div className="min-w-[66px] lgMin:min-w-[85px] lgMax:pt-7">
-                <img
-                  className="w-[66px] h-[66px] xlMin:w-[85px] xlMin:h-[85px] rounded-full"
-                  src={pictures[0]}
-                  alt={title}
-                />
-              </div>
+      {data.map(
+        ({ id, createdAt, title, pictures, location, name, address }) => {
+          return (
+            <li
+              key={id}
+              className="transition-all hover:scale-105 bg-cardBg xlMin:bg-white shadow-lg mb-2 last:mb-0  rounded-lg px-4 pt-[13px] xlMin:py-[24px] pb-[27px] xlMin:relative"
+            >
+              <Link className="flex" to={`/jobs-list/${id}`}>
+                <div className="min-w-[66px] lgMin:min-w-[85px] lgMax:pt-7">
+                  <img
+                    className="w-[66px] h-[66px] xlMin:w-[85px] xlMin:h-[85px] rounded-full"
+                    src={pictures[0]}
+                    alt={title}
+                  />
+                </div>
 
-              <div className="w-[100%] xlMin:w-[750px] ml-[19px] xlMin:ml-[26px]">
-                <div className="lgMax:flex lgMax:items-center lgMax:justify-between lgMax:mb-[14px]">
-                  <JobsItemIcons />
-                  <p className="font-light xlMin:font-normal text-sm xlMin:text-base xlMin:absolute right-4 bottom-6 text-secondaryTextColor">
-                    {convertDate(createdAt)}
+                <div className="w-[100%] xlMin:w-[750px] ml-[19px] xlMin:ml-[26px]">
+                  <div className="lgMax:flex lgMax:items-center lgMax:justify-between lgMax:mb-[14px]">
+                    <JobsItemIcons />
+                    <p className="font-light xlMin:font-normal text-sm xlMin:text-base xlMin:absolute right-4 bottom-6 text-secondaryTextColor">
+                      {convertDate(createdAt)}
+                    </p>
+                  </div>
+
+                  <p className="mb-[5px] xlMin:mb-2 font-normal xlMin:font-bold text-lg xlMin:text-xl text-primaryTextColor">
+                    {title}
                   </p>
+                  <p className="mb-[7px] font-normal text-base text-secondaryTextColor">
+                    {name}
+                  </p>
+                  <div className="flex items-center">
+                    <svg width={13} height={18}>
+                      <use href={`${sprite}#location`}></use>
+                    </svg>
+                    <p className="ml-2 font-normal text-base text-secondaryTextColor">
+                      {address}
+                    </p>
+                  </div>
                 </div>
-
-                <p className="mb-[5px] xlMin:mb-2 font-normal xlMin:font-bold text-lg xlMin:text-xl text-primaryTextColor">
-                  {title}
-                </p>
-                <p className="mb-[7px] font-normal text-base text-secondaryTextColor">
-                  {name}
-                </p>
-                <div className="flex items-center">
-                  <svg width={13} height={18}>
-                    <use href={`${sprite}#location`}></use>
-                  </svg>
-                  <p className="ml-2 font-normal text-base text-secondaryTextColor">{`Lat: ${location.lat} Long: ${location.long}`}</p>
-                </div>
-              </div>
-            </Link>
-          </li>
-        );
-      })}
+              </Link>
+            </li>
+          );
+        }
+      )}
     </>
   );
 };
